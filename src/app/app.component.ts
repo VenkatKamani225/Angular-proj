4b56ae6935a76a340d5c1bc08c159fcf75ddb2a5
VenkatKamani225 <venkatkamani2250@gmail.com>
2025-06-15 00:02:57 -0500

    Bindings - interpolation, property and event and two-way

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-proj';
+
+  public course ='Angular from Simpli';
+  public image ='https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png';
+
+  onClick(){
+    console.log('Event Binding from view to component');
+  }
+
+  public textValue =""
 }
