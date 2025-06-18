c173164acfcee9d22c2b32dc8395496ad51d366b
VenkatKamani225 <venkatkamani2250@gmail.com>
2025-06-17 23:13:38 -0500

    Added a sample typescript file with nametypes, classes, modules, objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponentComponent } from './form-component/form-component.component';
-// import { FormComponentComponent } from './form-component/form-component.component';
 
 @NgModule({
   declarations: [
